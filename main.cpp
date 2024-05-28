6a50fdea46a53e7b360c394254161aa49337403a
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-28 12:26:30 +0530

    calling training function inside main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include "iostream"
 #include <vector>
-#include <cmath>
-#include <cstdlib>
 #include "NeuralNetwork.h"
+#include "Utils.h"
 
 using namespace std;
 
@@ -13,5 +12,9 @@ int main()
         vector<int> topology = {2, 4, 1}; // More complex topology with one hidden layer of 4 neurons
         NeuralNetwork nn(topology);
 
+        vector<vector<double>> training_inputs = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        vector<vector<double>> training_outputs = {{0}, {1}, {1}, {0}};
+
+        nn.train(training_inputs, training_outputs, 1000, 0.1);
         return 0;
 }
