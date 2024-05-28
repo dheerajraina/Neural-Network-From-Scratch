d5403b7e39d4ac21e445b58d2e5001291aab46b5
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-28 13:56:51 +0530

    calling it all in main

--- main.cpp
+++ main.cpp
@@ -15,9 +15,15 @@ int main()
         generate_and_store_data("data.csv", 10000);
         load_data("data.csv", training_inputs, training_outputs);
 
-        vector<int> topology = {2, 4, 1}; // each element represents one layer with respective number of units (neurons)
+        vector<int> topology = {2, 32, 32, 1}; // each element represents one layer with respective number of units (neurons)
         NeuralNetwork nn(topology);
 
-        nn.train(training_inputs, training_outputs, 50, 0.1);
+        nn.train(training_inputs, training_outputs, 80, 0.1);
+
+        for (const auto &input : training_inputs)
+        {
+                std::vector<double> output = nn.predict(input);
+                std::cout << "Input: " << input[0] << " " << input[1] << " -> Output: " << output[0] << std::endl;
+        }
         return 0;
 }
