87c0f1de33cd80a7f8b103b630be263ef2fd9b2f
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-28 13:27:42 +0530

    Added utility to generate XOR dataset

--- Utils.cpp
+++ Utils.cpp
@@ -1,6 +1,8 @@
 #include "Utils.h"
 #include <iostream>
 #include <random>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 double generate_random_number(double min, double max)
@@ -25,4 +27,26 @@ double sigmoid(double x)
 double sigmoid_derivative(double x)
 {
         return x * (1.0 - x);
+}
+
+void generate_and_store_data(const string &filename, int num_examples)
+{
+        ofstream file(filename);
+        if (!file.is_open())
+        {
+                cerr << "Unable to open file for writing!" << endl;
+                return;
+        }
+
+        srand(time(0));
+        for (int i = 0; i < num_examples; ++i)
+        {
+                int input1 = rand() % 2;
+                int input2 = rand() % 2;
+                int output = input1 ^ input2;
+                file << input1 << "," << input2 << "," << output << "\n";
+        }
+
+        file.close();
+        cout << "Data generated and stored in " << filename << endl;
 }
--- Utils.h
+++ Utils.h
@@ -1,7 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
+#include <string>
+#include <vector>
 
 double generate_random_number(double min, double max);
+
+// generate XOR dataset
+void generate_and_store_data(const std::string &filename, int num_examples);
 
 // Activation functions
 double sigmoid(double x);
