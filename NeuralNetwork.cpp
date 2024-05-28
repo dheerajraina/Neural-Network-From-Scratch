23e3e9ebe1c51093985cbd46bd261f0794c181d7
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-28 12:14:23 +0530

    added backward propagation method

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -50,3 +50,49 @@ void NeuralNetwork::forward(const vector<double> &input)
                 }
         }
 }
+
+void NeuralNetwork::backward(const vector<double> &input, const vector<double> &expected_output, double learning_rate)
+{
+        vector<vector<double>> deltas(topology.size());
+
+        // Output layer delta
+        for (int i = 0; i < topology.back(); ++i)
+        {
+                double error = expected_output[i] - layers.back()[i];
+                deltas.back().push_back(error * sigmoid_derivative(layers.back()[i]));
+        }
+
+        // Hidden layers delta
+        for (int i = topology.size() - 2; i > 0; --i)
+        {
+                for (int j = 0; j < topology[i]; ++j)
+                {
+                        double error = 0.0;
+                        for (int k = 0; k < topology[i + 1]; ++k)
+                        {
+                                error += deltas[i + 1][k] * weights[i][j][k];
+                        }
+                        deltas[i].push_back(error * sigmoid_derivative(layers[i][j]));
+                }
+        }
+
+        // Update weights and biases
+        for (int i = 0; i < topology.size() - 1; ++i)
+        {
+                for (int j = 0; j < topology[i]; ++j)
+                {
+                        for (int k = 0; k < topology[i + 1]; ++k)
+                        {
+                                weights[i][j][k] += learning_rate * deltas[i + 1][k] * layers[i][j];
+                        }
+                }
+        }
+
+        for (int i = 1; i < topology.size(); ++i)
+        {
+                for (int j = 0; j < topology[i]; ++j)
+                {
+                        biases[i][j] += learning_rate * deltas[i][j];
+                }
+        }
+}
